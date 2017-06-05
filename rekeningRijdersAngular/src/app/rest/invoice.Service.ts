f29cac0a6205b84fbc8e0e851e1cee9ea648c11d
Thaencharun,lino S <l.thaencharun@student.fontys.nl>
2017-06-05 16:44:05 +0200

    change

--- rekeningRijdersAngular/src/app/rest/invoice.Service.ts
+++ rekeningRijdersAngular/src/app/rest/invoice.Service.ts
@@ -28,7 +28,7 @@ export class InvoiceService {
     SetInvoicePaid(id: any) : Promise<Invoice>{
         var header = new Headers();
         header.append('Content-Type', 'application/json');
-        return this.http.put(this.localurl + 'InvoicePaid', JSON.stringify(id), {headers: header})
+        return this.http.post(this.localurl + 'InvoicePaid', JSON.stringify(id), {headers: header})
                         .toPromise()
                         .then(this.extractData);
     }
